e2dad34ebc1ec70b24183f5bd64efb435765875a
servernablydatel <servernablydatel@gmail.com>
2026-07-21 13:15:56 +0400

    Update VanillaHead.java

--- src/main/java/com/example/vanillahead/VanillaHead.java
+++ src/main/java/com/example/vanillahead/VanillaHead.java
@@ -1,36 +1,37 @@
 package com.example.vanillahead;
 
-import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.ChatColor;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class VanillaHead extends JavaPlugin implements CommandExecutor {
+public class VanillaHead extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("VanillaHead plugin enabled!");
-        // Регистрируем команду /head и говорим, что её обрабатывает этот же класс
-        getCommand("head").setExecutor(this);
+        // Получаем команду из plugin.yml
+        PluginCommand cmd = getCommand("vanillahead");
+        
+        // Если команда прописана в plugin.yml, вешаем на неё обработчик
+        if (cmd != null) {
+            cmd.setExecutor((sender, command, label, args) -> {
+                if (sender instanceof Player player) {
+                    player.sendMessage(ChatColor.GREEN + "✅ VanillaHead успешно работает!");
+                    return true;
+                } else {
+                    sender.sendMessage("Эту команду можно использовать только в игре.");
+                    return true;
+                }
+            });
+        } else {
+            getLogger().warning("Команда 'vanillahead' не найдена в plugin.yml! Проверьте отступы.");
+        }
+
+        getLogger().info("🟩 VanillaHead plugin enabled!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("VanillaHead plugin disabled!");
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player player) {
-            ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
-            player.getInventory().addItem(skull);
-            player.sendMessage("§6Ты получил голову!");
-        } else {
-            sender.sendMessage("Эту команду можно использовать только в игре.");
-        }
-        return true;
+        getLogger().info("🟥 VanillaHead plugin disabled!");
     }
 }
